450f5f261577e5e21087f9c44f53bd8cae6b9d51
Bian Klančnik <bian.klancnik@gmail.com>
2018-03-05 17:49:06 +0000

    Prvi commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,27 @@
+class CommentsController < ApplicationController
+    before_action :authenticate_user!, except: [:index, :show]
+    
+    def new
+        @comment = current_user.comments.build
+    end
+    
+    def create
+        @comment = current_user.comments.build(comment_params)
+        @post = Post.find(params[:post_id])
+        @comment.post_id = @post.id
+        @comment.save
+        redirect_to post_path(@post)
+    end
+    
+    def destroy
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.find(params[:id])
+        @comment.destroy
+        
+        redirect_to post_path(@post)
+    end
+    
+    private def comment_params
+        params.require(:comment).permit(:body, :user_id, :post_id)
+    end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,53 @@
+class PostsController < ApplicationController
+    before_action :authenticate_user!, except: [:index]
+    def index
+        @posts = Post.all
+    end
+    
+    def new
+        @post = Post.new
+    end
+    
+    def show
+        @post = Post.find(params[:id])
+    end
+    
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update
+        @post = Post.find(params[:id])
+        if(@post.update(post_params))
+            redirect_to @post
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @post = Post.find(params[:id])
+        if @post.destroy
+      flash[:notice] = "Successfully deleted post!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "Error updating post!"
+    end
+    end
+    
+    def create
+        #render plain: params[:post].inspect
+        @post = Post.new(post_params)
+        @post.user_id = current_user.id
+        
+        if(@post.save)
+            redirect_to @post
+        else
+            render 'new'
+        end
+    end
+
+    private def post_params
+        params.require(:post).permit(:title, :body, :image, :user_id)
+    end
+end
